758f3e60053619ad2c45180f4cb5d428ece27c79
Ink-Song <sunruh@proton.me>
2025-12-28 20:13:20 +0100

    Add image to wumbology

--- javascript/button.js
+++ javascript/button.js
@@ -1,20 +1,25 @@
-var modal = document.getElementById("modalThing");
+var popup = document.getElementById("popup");
 var button = document.getElementById("openButton");
-var span = document.getElementById("close");
+var closeBtn = document.getElementById("closeBtn");
 
 // When the user clicks the button, open the modal
-button.onclick = function() {
-  modal.style.display = "block";
-}
+button.addEventListener('click', () => {
+    popup.showModal();
+}); 
 
-// When the user clicks on (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+closeBtn.addEventListener('click', () => {
+  popup.close();
+});
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
+// // When the user clicks on (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
